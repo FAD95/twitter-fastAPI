d721b00418a62a2b05a1280ba89c42c1980e3ad2
FAD95 <feescorciam@protonmail.com>
2022-02-15 14:28:12 -0500

    Post a tweet

--- main.py
+++ main.py
@@ -201,8 +201,38 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"],
 )
-def post():
-    pass
+def post(tweet: Tweet = Body(...)):
+    """
+    Post a tweet
+
+    This path will post a new tweet
+
+    Parameters:
+        - Request body:
+            - tweet: Tweet
+
+    Returns a json with the new tweet information:
+        - tweet_id: UUID
+        - content: str
+        - created_at: datetime
+        - updated_at: Optional[datetime]
+        - by: User
+    """
+
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.load(f)
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        if "updated_at" in tweet_dict.keys():
+            tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+    return tweet
+
 
 
 ### Show a tweet
